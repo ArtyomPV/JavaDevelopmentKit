2e907357f308f339185efda55ce67483824c2939
Artyom <itcoding76@gmail.com>
2023-11-15 07:54:46 +0200

    добавлен компонент JPanel с двумя кнопками

--- src/main/java/ru/gb/jdk/lecture01/game/GameWindow.java
+++ src/main/java/ru/gb/jdk/lecture01/game/GameWindow.java
@@ -1,18 +1,31 @@
 package ru.gb.jdk.lecture01.game;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow  extends JFrame {
     private static final int WINDOW_HEIGHT = 555;
     private static final int WINDOW_WIDTH = 507;
     private static final int WINDOW_POSX = 800;
     private static final int WINDOW_POSY = 300;
+
+    JButton btnStart = new JButton("New Game");
+    JButton btnExit = new JButton("Exit");
+
     public GameWindow(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(WINDOW_POSX, WINDOW_POSY);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("TicTacToe");
         setResizable(false);
+
+        JPanel panBottom = new JPanel(new GridLayout(1, 2));
+        panBottom.add(btnStart);
+        panBottom.add(btnExit);
+        add(panBottom, BorderLayout.SOUTH);
+
         setVisible(true);
+
+
     }
 }
