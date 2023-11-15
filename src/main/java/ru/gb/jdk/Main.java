7b68ddd90308d00401c8d7f10f3ae5b901ac7611
Artyom <itcoding76@gmail.com>
2023-11-15 06:58:11 +0200

    Создано графическое приложение с размерами и расположением на экране

--- src/main/java/ru/gb/jdk/Main.java
+++ src/main/java/ru/gb/jdk/Main.java
@@ -1,7 +1,10 @@
 package ru.gb.jdk;
 
+import ru.gb.jdk.lecture01.game.GameWindow;
+
 public class Main {
     public static void main(String[] args) {
+        new GameWindow();
         System.out.println("Hello world!");
     }
 }
--- src/main/java/ru/gb/jdk/lecture01/game/GameWindow.java
+++ src/main/java/ru/gb/jdk/lecture01/game/GameWindow.java
@@ -1,4 +1,17 @@
 package ru.gb.jdk.lecture01.game;
 
-public class GameWindow {
+import javax.swing.*;
+
+public class GameWindow  extends JFrame {
+    private static final int WINDOW_HEIGHT = 555;
+    private static final int WINDOW_WIDTH = 507;
+    private static final int WINDOW_POSX = 800;
+    private static final int WINDOW_POSY = 300;
+    public GameWindow(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(WINDOW_POSX, WINDOW_POSY);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        setVisible(true);
+    }
 }
