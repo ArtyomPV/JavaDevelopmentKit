ccf71fd2ee34ac93533609d59c6af90351be0ed3
Artyom <itcoding76@gmail.com>
2023-11-17 07:15:22 +0200

    игра в крестики нолики с примитивным ИИ завершена

--- src/main/java/ru/gb/jdk/lecture01/game/Map.java
+++ src/main/java/ru/gb/jdk/lecture01/game/Map.java
@@ -2,20 +2,212 @@ package ru.gb.jdk.lecture01.game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class Map extends JPanel {
-    Map(){
+    private static final Random RANDOM = new Random();
+    private static final int HUMAN_DOT = 1;
+    private static final int AI_DOT = 2;
+    private static final int EMPTY_DOT = 0;
+    private static final int DOT_PADDING = 5;
+
+    private int gameOverType;
+    private static final int STATE_DRAW = 0;
+    private static final int STATE_WIN_HUMAN = 1;
+    private static final int STATE_WIN_AI = 2;
+    private static final String MSG_WIN_HUMAN = "Победил игрок!";
+    private static final String MSG_WIN_AI = "Победил компьютер!";
+    private static final String MSG_DRAW = "Ничья!";
+
+    private final int fieldSizeX = 3;           // размер поля по оси X
+    private final int fieldSizeY = 3;           // размер поля по оси Y
+    private char[][] field;
+    private int panelWidth;                     // ширина поля в пх
+    private int panelHeght;                     // высота поля в пх
+    private int cellWidth;                      // ширина ячейки в пх
+    private int cellHeght;                      // высота ячейки в пх
+    private boolean isGameOver;
+    private boolean isInitialized;
+
+    Map() {
+
 //        setBackground(Color.BLACK);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                update(e);
+            }
+        });
+        isInitialized = false;
     }
-    public void startNewGame(int mode, int fSzX, int fSzY, int wLen){
-        System.out.printf("Mode: %d;\nSize: x=%d, y=%d;\nWindow length: %d", mode, fSzX, fSzY, wLen);
+
+    /**
+     * Проверяем еслми не закончена игра или игра про инициализирована
+     * вычисляем координаты ячейки по оси X и Y
+     * проверяем, что ячейка валидна (не занята) и пустая
+     * данной ячейке присваиваем значение хода игрока
+     * @param e действие мышки
+     */
+    private void update(MouseEvent e) {
+        if(isGameOver || !isInitialized) return;
+        int cellX = e.getX() / cellWidth;
+        int cellY = e.getY() / cellHeght;
+        System.out.println(cellX);
+        System.out.println(cellY);
+        if (!isValidCell(cellX, cellY) || !isEmptyCell(cellX, cellY)) {
+            System.out.println("Error");
+            return;
+        }
+        field[cellY][cellX] = HUMAN_DOT;
+        System.out.printf("x=%d, y=%d\n", cellX, cellY);
+        repaint();
+
+        if(checkEndGame(HUMAN_DOT, STATE_WIN_HUMAN)) return;
+
+        aiTurn();
+        repaint();
+        if(checkEndGame(AI_DOT, STATE_WIN_AI)) return;
     }
+
+    private boolean checkEndGame(int dot, int gameOverType){
+        if(checkWin((char) dot)){
+            this.gameOverType = gameOverType;
+            isGameOver = true;
+            repaint();
+            return true;
+        }
+        if(isMapFull()){
+            this.gameOverType = STATE_DRAW;
+            isGameOver = true;
+            repaint();
+            return  true;
+        }
+        return false;
+    }
+
+    public void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
+        initMap();
+        isGameOver = false;
+        isInitialized = true;
+        System.out.printf("Mode: %d;\nSize: x=%d, y=%d;\nWindow length: %d\n", mode, fSzX, fSzY, wLen);
+        repaint();
+    }
+
     @Override
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         render(g);
     }
-    private void render(Graphics g){
 
+    private void render(Graphics g) {
+        if(!isInitialized) return;
+        g.setColor(Color.BLACK);
+
+        panelWidth = getWidth();
+        panelHeght = getHeight();
+        cellHeght = panelHeght / 3;
+        cellWidth = panelWidth / 3;
+
+        for (int h = 0; h < 3; h++) {
+            int y = h * cellHeght;
+            g.drawLine(0, y, panelWidth, y);
+        }
+        for (int w = 0; w < 3; w++) {
+            int x = w * cellWidth;
+            g.drawLine(x, 0, x, panelHeght);
+        }
+
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == EMPTY_DOT) continue;
+                if (field[y][x] == HUMAN_DOT) {
+                    g.setColor(Color.BLUE);
+                    g.fillOval(x * cellWidth + DOT_PADDING,
+                            y * cellHeght + DOT_PADDING,
+                            cellWidth - DOT_PADDING * 2,
+                            cellHeght - DOT_PADDING * 2);
+                } else if (field[y][x] == AI_DOT) {
+                    g.setColor(new Color(0xFF0000));
+                    g.fillOval(x * cellWidth + DOT_PADDING,
+                            y * cellHeght + DOT_PADDING,
+                            cellWidth - DOT_PADDING * 2,
+                            cellHeght - DOT_PADDING * 2);
+                } else {
+                    throw new RuntimeException("Unexpected value " + field[y][x] + " in cell: x=" + x + " y=" + y);
+                }
+            }
+        }
+
+        if(isGameOver) showMessageGameOver(g);
+    }
+
+    private void showMessageGameOver(Graphics g) {
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(0, 200, getWidth(), 70);
+        g.setColor(Color.YELLOW);
+        g.setFont(new Font("Times new roman", Font.BOLD, 48));
+        switch(gameOverType) {
+            case STATE_DRAW:
+                g.drawString(MSG_DRAW, 180, getHeight()/2); break;
+            case STATE_WIN_AI:
+                g.drawString(MSG_WIN_AI, 20, getHeight()/2); break;
+            case STATE_WIN_HUMAN:
+                g.drawString(MSG_WIN_HUMAN, 70, getHeight()/2); break;
+            default:
+                throw new RuntimeException("Unexpected gameOver state: " + gameOverType);
+        }
+    }
+
+    private void initMap() {
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                field[i][j] = EMPTY_DOT;
+            }
+        }
+    }
+
+    private boolean isValidCell(int x, int y) {
+        System.out.println("Is not valid");
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    private boolean isEmptyCell(int x, int y) {
+        System.out.println("is empty cell");
+        return field[y][x] == EMPTY_DOT;
+    }
+
+    private void aiTurn() {
+        int x, y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isEmptyCell(x, y));
+        field[y][x] = AI_DOT;
+    }
+
+    private boolean checkWin(char c) {
+        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
+        if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
+        if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
+
+        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) return true;
+        if (field[0][1] == c && field[1][1] == c && field[2][1] == c) return true;
+        if (field[0][2] == c && field[1][2] == c && field[2][2] == c) return true;
+
+        if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
+        if (field[2][0] == c && field[1][1] == c && field[0][2] == c) return true;
+        return false;
+    }
+
+    private boolean isMapFull() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (field[i][j] == EMPTY_DOT) return false;
+            }
+        }
+        return true;
     }
 }
