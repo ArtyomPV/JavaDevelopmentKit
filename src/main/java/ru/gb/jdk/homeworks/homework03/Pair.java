611adf019335598ddea0b4424035f285230426b6
Artyom <itcoding76@gmail.com>
2023-12-07 07:46:05 +0200

    HomeWorks 03 added task04 (simple class Pair)

---
+++ src/main/java/ru/gb/jdk/homeworks/homework03/Pair.java
@@ -0,0 +1,27 @@
+package ru.gb.jdk.homeworks.homework03;
+
+public class Pair<T1, T2> {
+    private T1 first;
+    private T2 second;
+
+    public Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
